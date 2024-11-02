ad6de12a8dd7e9d1173aaee4e3cd6ac86ce97684
“Sushmoy <learnsushmoysk1091@gmail.com>
2024-11-03 00:19:14 +0600

    add college

---
+++ college.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE college1;
+USE college1;
+
+CREATE TABLE student1(
+	rollno INT PRIMARY KEY,
+    name VARCHAR(50),
+    marks INT NOT NULL,
+    grade VARCHAR(1),
+    city VARCHAR(20)
+);
+
+INSERT INTO student1
+(rollno,name,marks,grade,city)
+VALUES
+(101,"Sushmoy",78,"C","Dhaka"),
+(102,"Nandi",93,"A","Jashore"),
+(103,"Bappi",85,"B","Kushtia"),
+(104,"Bijoy",96,"A","Chittagong"),
+(105,"Kanchon",60,"D","Satkhira"),
+(106,"Surjo",82,"B","ShreeMongol");
+
+
+SELECT name, marks FROM student1;
+SELECT DISTINCT city FROM student1;
+SELECT grade FROM student1;
+
+
+
