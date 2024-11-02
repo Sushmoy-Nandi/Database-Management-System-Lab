0739e69c422077fb4cbb6ef4cc2c586124b6a0e5
“Sushmoy <learnsushmoysk1091@gmail.com>
2024-11-02 12:11:19 +0600

    SQL added

---
+++ SQL.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE college;
+
+USE college;
+
+CREATE TABLE student(
+	rollno INT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+SELECT * FROM student;
+
+CREATE DATABASE xyz_company;
+USE xyz_company;
+
+CREATE TABLE employee(
+	id INT PRIMARY KEY,
+    name VARCHAR(100),
+    salary INT
+);
+
+INSERT INTO employee
+(id,name,salary)
+VALUES
+(1,"Sushmoy",25000),
+(2,"Nandi",30000),
+(3,"Bappi",40000);
+
+CREATE TABLE temp1(
+	id INT UNIQUE
+);
+
+
+INSERT INTO temp1 VALUES(101);
+INSERT INTO temp1 VALUES(101);
+ 
+SELECT * FROM temp1;
+
+CREATE TABLE temp1(
+	id INT,
+    name VARCHAR(50),
+    city VARCHAR(20),
+    PRIMARY KEY(id, name)
+);
+
+
+
+
+
+
+
+
+
+
